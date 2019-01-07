5fc9e4784ba5e584ec98d44683b4fe9a3be6ae66
Symbolics <steve@nunez.org>
2019-01-07 17:14:08 +0800

    Cleaned up formatting

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-* Chunga for Genera
+# Chunga for Genera
 
 The upstream maintainer refuses pull requests for Genera support, so maintaining a separate fork here.
--- specials.lisp
+++ specials.lisp
@@ -1,31 +1,4 @@
-;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CHUNGA; Base: 10 -*-
-;;; $Header: /usr/local/cvsrep/chunga/specials.lisp,v 1.12 2008/05/24 03:06:22 edi Exp $
-
-;;; Copyright (c) 2006-2010, Dr. Edmund Weitz.  All rights reserved.
-
-;;; Redistribution and use in source and binary forms, with or without
-;;; modification, are permitted provided that the following conditions
-;;; are met:
-
-;;;   * Redistributions of source code must retain the above copyright
-;;;     notice, this list of conditions and the following disclaimer.
-
-;;;   * Redistributions in binary form must reproduce the above
-;;;     copyright notice, this list of conditions and the following
-;;;     disclaimer in the documentation and/or other materials
-;;;     provided with the distribution.
-
-;;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
-;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CHUNGA; Base: 10 -*-
 
 (in-package :chunga)
 
@@ -48,8 +21,7 @@
 				;; #\Return and #\Linefeed do not map to the standard
 				;; codepoints.  Explicitly list those codepoints here.
 				#+:genera '(#o15 #o12))
-  "A 2-element array consisting of the character codes for a CRLF
-sequence.")
+  "A 2-element array consisting of the character codes for a CRLF sequence.")
 
 (define-constant +hex-digits+ '#.(coerce "0123456789ABCDEF" 'list)
   "The hexadecimal digits.")
@@ -79,8 +51,7 @@ combine them with the next header.  This will only be done if this
 variable has a true value, though.")
 
 (defvar *char-buffer* nil
-  "A `buffer' for one character.  Used by PEEK-CHAR* and
-UNREAD-CHAR*.")
+  "A `buffer' for one character.  Used by PEEK-CHAR* and UNREAD-CHAR*.")
 
 (pushnew :chunga *features*)
 
